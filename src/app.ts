7514ba91259fc3b10733c2af70aa2f2e734470e8
Max Verigin <max.verigin@gmail.com>
2022-03-31 17:50:15 -0700

    basic lambda logic

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,17 @@
-export const lambdaHandler = async (event): Promise<void> => {
+import { EventBridgeEvent } from 'aws-lambda';
+import axios from 'axios';
+import { parse } from 'node-html-parser';
+
+export const lambdaHandler = async (event : EventBridgeEvent<any, any>): Promise<void> => {
+    const url = process.env.QUERY_URL || '';
+    const querySelector = process.env.QUERY_SELECTOR || '';
+    const res = await axios.get(url);
+    const html = res.data;
+    const root = parse(html);
+    const match = root.querySelector(querySelector);
+    if (match) {
+        console.log(`Matched selector ${querySelector}`);
+    } else {
+        console.log(`No match for selector ${querySelector}`);
+    }
 };
