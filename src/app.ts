bee658e8cb6d32db4c552f84832ddd04614c2d65
Max Verigin <max.verigin@gmail.com>
2022-03-31 18:33:36 -0700

    basic logic and config

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,22 @@ import { parse } from 'node-html-parser';
 
 export const lambdaHandler = async (event : EventBridgeEvent<any, any>): Promise<void> => {
     const url = process.env.QUERY_URL || '';
+    if (!url) {
+        throw new Error("No QUERY_URL provided");
+    }
+
     const querySelector = process.env.QUERY_SELECTOR || '';
+    if (!querySelector) {
+        throw new Error("No QUERY_SELECTOR provided");
+    }
+
     const res = await axios.get(url);
     const html = res.data;
     const root = parse(html);
     const match = root.querySelector(querySelector);
     if (match) {
-        console.log(`Matched selector ${querySelector}`);
+        const elementHtml = match.outerHTML;
+        console.log(`Matched selector ${querySelector} with element: ${elementHtml}`);
     } else {
         console.log(`No match for selector ${querySelector}`);
     }
